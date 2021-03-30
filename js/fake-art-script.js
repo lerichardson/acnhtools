090993d8353f434ab18eab397b959e6b3c02be40
Levi Richardson <litbelb1@outlook.com>
2021-03-30 16:58:34 -0700

    Add all main files

---
+++ js/fake-art-script.js
@@ -0,0 +1,139 @@
+      change(0);
+        function change(index) {
+          if (index == 1) {
+            document.getElementById("nextbutton").style.cursor = "default"
+            document.getElementById("link").href = "/paintings/academic-painting.html"
+          }
+          if (index == 2) {
+            document.getElementById("nextbutton").style.cursor = "default"
+            document.getElementById("link").href = "/paintings/amazing-painting.html"
+          }
+            if (index == 3) {
+            document.getElementById("nextbutton").style.cursor = "default"
+            document.getElementById("link").href = "/paintings/basic-painting.html"
+          } if (index == 4) {
+            document.getElementById("nextbutton").style.cursor = "default"
+            document.getElementById("link").href = "/paintings/calm-painting.html"
+          } if (index == 5) {
+            document.getElementById("nextbutton").style.cursor = "default"
+            document.getElementById("link").href = "/paintings/common-painting.html"
+          } if (index == 6) {
+            document.getElementById("nextbutton").style.cursor = "default"
+            document.getElementById("link").href = "/paintings/detailed-painting.html"
+          } if (index == 7) {
+            document.getElementById("nextbutton").style.cursor = "default"
+            document.getElementById("link").href = "/paintings/dynamic-painting.html"
+          }
+            if (index == 8) {
+            document.getElementById("nextbutton").style.cursor = "default"
+            document.getElementById("link").href = "/paintings/famous-painting.html"
+          } if (index == 9) {
+            document.getElementById("nextbutton").style.cursor = "default"
+            document.getElementById("link").href = "/paintings/flowery-painting.html"
+          } if (index == 10) {
+            document.getElementById("nextbutton").style.cursor = "default"
+            document.getElementById("link").href = "/paintings/glowing-painting.html"
+          } if (index == 11) {
+            document.getElementById("nextbutton").style.cursor = "default"
+            document.getElementById("link").href = "/paintings/graceful-painting.html"
+          } if (index == 12) {
+            document.getElementById("nextbutton").style.cursor = "default"
+            document.getElementById("link").href = "/paintings/jolly-painting.html"
+          } if (index == 13) {
+            document.getElementById("nextbutton").style.cursor = "default"
+            document.getElementById("link").href = "/paintings/moody-painting.html"
+          } if (index == 14) {
+            document.getElementById("nextbutton").style.cursor = "default"
+            document.getElementById("link").href = "/paintings/moving-painting.html"
+          } if (index == 15) {
+            document.getElementById("nextbutton").style.cursor = "default"
+            document.getElementById("link").href = "/paintings/mysterious-painting.html"
+          }
+           if (index == 16) {
+            document.getElementById("nextbutton").style.cursor = "default"
+            document.getElementById("link").href = "/paintings/nice-painting.html"
+          }
+            if (index == 17) {
+            document.getElementById("nextbutton").style.cursor = "default"
+            document.getElementById("link").href = "/paintings/perfect-painting.html"
+          } if (index ==18) {
+            document.getElementById("nextbutton").style.cursor = "default"
+            document.getElementById("link").href = "/paintings/proper-painting.html"
+          } if (index == 19) {
+            document.getElementById("nextbutton").style.cursor = "default"
+            document.getElementById("link").href = "/paintings/quaint-painting.html"
+          } if (index == 20) {
+            document.getElementById("nextbutton").style.cursor = "default"
+            document.getElementById("link").href = "/paintings/scary-painting.html"
+          } if (index == 21) {
+            document.getElementById("nextbutton").style.cursor = "default"
+            document.getElementById("link").href = "/paintings/scenic-painting.html"
+          } if (index == 22) {
+            document.getElementById("nextbutton").style.cursor = "default"
+            document.getElementById("link").href = "/paintings/serene-painting.html"
+          } if (index == 23) {
+            document.getElementById("nextbutton").style.cursor = "default"
+            document.getElementById("link").href = "/paintings/sinking-painting.html"
+          } if (index == 24) {
+            document.getElementById("nextbutton").style.cursor = "default"
+            document.getElementById("link").href = "/paintings/solemn-painting.html"
+          } if (index == 25) {
+            document.getElementById("nextbutton").style.cursor = "default"
+            document.getElementById("link").href = "/paintings/twinkling-painting.html"
+          } if (index == 26) {
+            document.getElementById("nextbutton").style.cursor = "default"
+            document.getElementById("link").href = "/paintings/warm-painting.html"
+          } if (index == 27) {
+            document.getElementById("nextbutton").style.cursor = "default"
+            document.getElementById("link").href = "/paintings/worthy-painting.html"
+          } if (index == 28) {
+            document.getElementById("nextbutton").style.cursor = "default"
+            document.getElementById("link").href = "/sculptures/ancient-statue.html"
+            } if (index == 29) {
+            document.getElementById("nextbutton").style.cursor = "default"
+            document.getElementById("link").href = "/sculptures/beautiful-statue.html"
+          } if (index == 30) {
+            document.getElementById("nextbutton").style.cursor = "default"
+            document.getElementById("link").href = "/sculptures/familiar-statue.html"
+          } if (index == 31) {
+            document.getElementById("nextbutton").style.cursor = "default"
+            document.getElementById("link").href = "/sculptures/gallant-statue.html"
+          } if (index == 32) {
+            document.getElementById("nextbutton").style.cursor = "default"
+            document.getElementById("link").href = "/sculptures/great-statue.html"
+          } if (index == 33) {
+            document.getElementById("nextbutton").style.cursor = "default"
+            document.getElementById("link").href = "/sculptures/informative-statue.html"
+            } if (index == 34) {
+            document.getElementById("nextbutton").style.cursor = "default"
+            document.getElementById("link").href = "/sculptures/motherly-statue.html"
+          } if (index == 35) {
+            document.getElementById("nextbutton").style.cursor = "default"
+            document.getElementById("link").href = "/sculptures/mystic-statue.html"
+          } if (index == 36) {
+            document.getElementById("nextbutton").style.cursor = "default"
+            document.getElementById("link").href = "/sculptures/robust-statue.html"
+          } if (index == 37) {
+            document.getElementById("nextbutton").style.cursor = "default"
+            document.getElementById("link").href = "/sculptures/rock-head-statue.html"
+          } if (index == 38) {
+            document.getElementById("nextbutton").style.cursor = "default"
+            document.getElementById("link").href = "/sculptures/tremendous-statue.html"
+          } if (index == 39) {
+            document.getElementById("nextbutton").style.cursor = "default"
+            document.getElementById("link").href = "/sculptures/valiant-statue.html"
+          } if (index == 40) {
+            document.getElementById("nextbutton").style.cursor = "default"
+            document.getElementById("link").href = "/sculptures/warrior-statue.html"
+          } if (index == 41) {
+            document.getElementById("nextbutton").style.cursor = "default"
+            document.getElementById("link").href = "/paintingss/wild-painting-right.html"
+          } if (index == 42) {
+            document.getElementById("nextbutton").style.cursor = "default"
+            document.getElementById("link").href = "/paintingss/wild-painting-left.html"
+          if (index == 0) {
+            document.getElementById("nextbutton").style.cursor = "not-allowed"
+            document.getElementById("link").href = "#"
+          }
+          }
+        }
